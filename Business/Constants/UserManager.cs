bfc8e33149a071c0022a79c046416171813885ae
Seyyit <seyyitatim@gmail.com>
2021-04-22 01:07:10 +0300

    added User's Service & Manager & Dal.

---
+++ Business/Constants/UserManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Constants
+{
+    public class UserManager : IUserService
+    {
+        private readonly IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+            return _userDal.UserClaims(user);
+        }
+
+        public void Add(User user)
+        {
+            _userDal.Add(user);
+        }
+
+        public User GetByMail(string email)
+        {
+            return _userDal.Get(u => u.Email == email);
+        }
+    }
+}
