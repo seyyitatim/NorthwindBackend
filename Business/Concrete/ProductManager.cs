940ac9ea4f90dccfc444b2b22d0f434edfbc7aec
Seyyit <seyyitatim@gmail.com>
2021-04-21 18:18:13 +0300

    added magic string(Messages).

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -36,19 +37,19 @@ namespace Business.Concrete
         public IResult Add(Product product)
         {
             _productDal.Add(product);
-            return new SuccessResult("Ürün başarıyla eklendi.");
+            return new SuccessResult(Messages.ProductAdded);
         }
 
         public IResult Update(Product product)
         {
             _productDal.Update(product);
-            return new SuccessResult("Ürün başarıyla güncellendi.");
+            return new SuccessResult(Messages.ProductUpdated);
         }
 
         public IResult Delete(Product product)
         {
             _productDal.Delete(product);
-            return new SuccessResult("Ürün başarıyla silindi.");
+            return new SuccessResult(Messages.ProductDeleted);
         }
     }
 }
