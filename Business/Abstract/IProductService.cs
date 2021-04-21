05c60c12bf8fab53bdb0420f2e553fbf98272c09
Seyyit <seyyitatim@gmail.com>
2021-04-21 03:16:53 +0300

    Added IProductService and ProductManager.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetList();
+        List<Product> GetListByCategory(int categoryId);
+        Product GetById(int productId);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
