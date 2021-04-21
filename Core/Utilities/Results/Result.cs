700cdbd3735a3f9636e15857e6778d1ca7192c84
Seyyit <seyyitatim@gmail.com>
2021-04-21 18:04:52 +0300

    added Result system.

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success,string message):this(success)//burada diğer contructer'ı tetikliyor.
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
